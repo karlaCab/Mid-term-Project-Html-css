e218bc34f1fae1e9c7d823c1f071471472a8a513
krlaCabGr <33165827+karlaCab@users.noreply.github.com>
2023-07-03 14:25:26 -0700

    Add files via upload

---
+++ Final-project/js/toggle-menu.js
@@ -0,0 +1,8 @@
+function openMenu() {
+  const mobileMenu = document.getElementById("mobile-links");
+  mobileMenu.style.display = "block";
+}
+function closeMenu() {
+  const mobileMenu = document.getElementById("mobile-links");
+  mobileMenu.style.display = "none";
+}
